a63a180355bfd279ea51a255613b20faa8ab4160
Sina Keang <sina.keang@innovapost.com>
2018-06-26 16:16:39 -0400

    add todo list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { NgRedux, NgReduxModule } from '@angular-redux/store';
 import { IAppState, rootReducer, INITIAL_STATE } from './store';
 
 
 import { AppComponent } from './app.component';
 import { TodoOverviewComponent } from './todo-overview/todo-overview.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoOverviewComponent
+    TodoOverviewComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgReduxModule
   ],
   providers: [],
